4ad7b1160a54eeaff235dca6cd3dc1fafe935158
David Mahbubi <ulrichdavid0370@gmail.com>
2025-01-31 13:55:56 +0700

    feat: added replacer function

--- index.ts
+++ index.ts
@@ -1,1 +1,37 @@
-console.log("Hello via Bun!");
+import { readdir } from 'node:fs/promises'
+import { join } from 'path'
+import langFiles from './../public/lang/id-ID.json'
+
+const viewsPath: string = join('.', '..', 'resources', 'views')
+
+const views: string[] = await readdir(viewsPath, {
+  recursive: true
+})
+
+const viewFiles: string[] = views.filter((data: string): boolean => Bun.file(data).type === 'application/x-httpd-php')
+
+for (const viewFile of viewFiles) {
+
+  const file = Bun.file(join(viewsPath, viewFile))
+  const globalRegex: RegExp = /\{\{ ___\('([^']+)'(?:, \[(.*?)\])?\) \}\}/g
+
+  let text = await file.text()
+  const matchedText = text.match(globalRegex)
+
+  if (matchedText && matchedText.length > 0) {
+
+    for (const t of matchedText) {
+      let matchText: any = [ ...t.matchAll(/___\(['"]([^'"]+)['"]\)/g) ].map(match => match[1])
+      if (matchText && matchText.length > 0) {
+        matchText = matchText.map((m: any) => m in langFiles ? (langFiles as any)[m] : '').join(' ')
+        if (matchText && matchText != '') {
+          text.replace(globalRegex, matchText)
+        }
+      }
+    }
+  }
+
+  await file.write(text)
+
+  console.log(text)
+}
